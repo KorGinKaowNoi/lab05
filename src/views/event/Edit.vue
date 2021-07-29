f6766164481868112897de27adb3a096d1873454
GetThisDonut <thitisan_ch@elearning.cmu.ac.th>
2021-07-29 17:17:22 +0700

    add flash message for edit page

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,9 +1,40 @@
 <template>
+<div id="flashMessage" v-if="Gstore.flashMessage">
+  {{Gstore.flashMessage}}
+</div>
   <p>Edit the event here</p>
+  <button @click="Edit">Edit</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject:['Gstore'],
+  methods:{
+    Edit(){
+      this.Gstore.flashMessage='your data has been edited';
+      setTimeout(()=>{
+        this.Gstore.flashMessage=''
+      },3000)
+      this.$router.push({
+        name:'EventDetails',
+        params:{id:this.event.id}
+      })
+    }
+  }
 }
 </script>
+<style> 
+@keyframes yellowfade {
+  from{
+    background:yellow;
+  }
+  to{
+    background: transparent;
+  }
+}
+#flashMessage{
+  animation-name: yellowfade;
+  animation-duration: 3s;
+}
+</style>
